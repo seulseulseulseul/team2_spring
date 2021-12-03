105b4a83a3aebb8f47eba63e2161e9c0c14f6dc1
jhjh2 <95323550+jhjh2@users.noreply.github.com>
2021-12-04 00:15:19 +0900

    Add files via upload

--- team2_git/src/main/java/com/itwillbs/service/ReplyService.java
+++ team2_git/src/main/java/com/itwillbs/service/ReplyService.java
@@ -1,29 +1,5 @@
 package com.itwillbs.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import com.itwillbs.domain.PageDTO;
-import com.itwillbs.domain.ReplyDTO;
-
-@Service
-public interface ReplyService { //test
-	
-	public void insertBoard(ReplyDTO replyDTO);
-	
-	public List<ReplyDTO> getBoardList(int c_num, PageDTO pageDTO);
-	
-	public ReplyDTO getBoard(ReplyDTO replyDTO);
-	
-	public void updateBoard(ReplyDTO replyDTO);
-	
-	public void deleteBoard(ReplyDTO replyDTO);
-	
-	public void deleteBoardAll(ReplyDTO replyDTO);
-	
-	public Integer getBoardCount(int c_num);
-	
-	public ReplyDTO numCheck(ReplyDTO replyDTO);
+public class ReplyService {
 
 }
