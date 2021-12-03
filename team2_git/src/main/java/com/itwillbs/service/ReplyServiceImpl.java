45a6360e362afea524f423acef981f626a5e6305
dev <dev@DESKTOP-LO2GEAN>
2021-12-04 01:02:50 +0900

    test

--- team2_git/src/main/java/com/itwillbs/service/ReplyServiceImpl.java
+++ team2_git/src/main/java/com/itwillbs/service/ReplyServiceImpl.java
@@ -1,5 +1,41 @@
 package com.itwillbs.service;
 
+@Service
 public class ReplyServiceImpl implements ReplyService{
+	
+	@Inject
+	private ReplyDAO replyDAO;
+	
+	public void insertBoard(ReplyDTO replyDTO) {
+		
+	}
+	
+	public List<ReplyDTO> getBoardList(int c_num, PageDTO pageDTO) {
+		
+	}
+	
+	public ReplyDTO getBoard(ReplyDTO replyDTO) {
+		
+	}
+	
+	public void updateBoard(ReplyDTO replyDTO) {
+		
+	}
+	
+	public void deleteBoard(ReplyDTO replyDTO) {
+		
+	}
+	
+	public void deleteBoardAll(ReplyDTO replyDTO) {
+		
+	}
+	
+	public Integer getBoardCount(int c_num) {
+		
+	}
+	
+	public ReplyDTO numCheck(ReplyDTO replyDTO) {
+		
+	}
 
 }
