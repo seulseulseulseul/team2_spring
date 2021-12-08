a8dc1c050ef59a728448b4a70d93fc0bda8f1b7a
user <user@DESKTOP-0MHNHQV>
2021-12-08 17:53:10 +0900

    trainer insert 스프링작업 완료

---
+++ team2_git/src/main/java/com/itwill/controller/Trainer_MemberController.java
@@ -0,0 +1,129 @@
+package com.itwill.controller;
+
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.itwill.domain.CenterDTO;
+import com.itwill.domain.Trainer_MemberDTO;
+import com.itwill.service.Trainer_MemberService;
+
+@Controller
+public class Trainer_MemberController {
+
+	@Inject
+	private Trainer_MemberService trainer_memberService;
+
+	@RequestMapping(value = "/member/trainer_register", method = RequestMethod.GET)
+	public String trainer_register() {
+		
+		return "member/trainer_register"; // �̵��� ������
+	}
+
+	@RequestMapping(value = "/member/trainer_registerPro", method = RequestMethod.POST)
+	public String trainer_registerPro(Trainer_MemberDTO trainer_memberDTO) {
+
+		int result = trainer_memberService.trainer_IdCheck(trainer_memberDTO);
+		try {
+			if(result==1) {
+				return "member/trainer_register";
+			}else if(result ==0) {
+				trainer_memberService.trainer_register(trainer_memberDTO);
+			}
+		}catch(Exception e) {
+			throw new RuntimeException();
+		}
+		
+		return "redirect:/member/trainer_login";
+	}
+	
+	@RequestMapping(value = "/member/trainer_login", method = RequestMethod.GET)
+	public String trainer_login() {
+		
+		return "member/trainer_login";
+	}
+	
+	@RequestMapping(value = "/member/trainer_loginPro", method = RequestMethod.POST)
+	public String trainer_loginPro(Trainer_MemberDTO trainer_memberDTO, HttpSession session) {
+
+		System.out.println("trainer_MemberController trainer_loginPro ");
+		System.out.println(trainer_memberDTO.getT_id());
+		System.out.println(trainer_memberDTO.getT_pass());
+		
+		Trainer_MemberDTO trainer_memberDTO2=trainer_memberService.trainer_userCheck(trainer_memberDTO);
+		
+		if(trainer_memberDTO2!=null) {
+	
+			session.setAttribute("u_id", trainer_memberDTO.getT_id());
+			session.setAttribute("id", "2");
+			return "redirect:/index";
+			
+		}else {
+			return "member/msg";
+		}
+		
+	}
+	
+	@RequestMapping(value = "/member/trainer_update", method = RequestMethod.GET)
+	public String trainer_update(HttpSession session,Model model) {
+	
+		String t_id=(String)session.getAttribute("t_id");
+	
+		Trainer_MemberDTO trainer_memberDTO=trainer_memberService.trainer_getMember(t_id);
+		
+		model.addAttribute("trainer_memberDTO", trainer_memberDTO);
+
+		return "member/trainer_update";
+	}
+	
+	@RequestMapping(value = "/member/trainer_updatePro", method = RequestMethod.POST)
+	public String trainer_updatePro(Trainer_MemberDTO trainer_memberDTO) {
+
+		System.out.println(" MemberController  updatePro ");
+	
+//		Trainer_MemberDTO trainer_memberDTO2=trainer_memberService.trainer_userCheck(trainer_memberDTO);
+				
+//		if(trainer_memberDTO2!=null) {
+			if(trainer_memberDTO!=null) {
+
+			trainer_memberService.trainer_updateMember(trainer_memberDTO);
+//			return "redirect:/member/main";
+			return "redirect:/index";
+					
+		}else {
+
+//			return "member/msg";
+			return "index";
+		}
+		
+	}
+	
+	//���̵� �ߺ� üũ
+	@ResponseBody
+	@RequestMapping(value="/member/trainer_IdCheck", method = RequestMethod.POST)
+	public int trainer_IdCheck(Trainer_MemberDTO trainer_memberDTO) {
+		int result = trainer_memberService.trainer_IdCheck(trainer_memberDTO);
+		return result;
+	}
+	
+	@RequestMapping(value = "/member/trainer_insert", method = RequestMethod.GET)
+	public String insert(){
+		return "trainer/insert";
+	}
+	
+	@RequestMapping(value = "/member/trainer_insertPro", method = RequestMethod.POST)
+	public String insertPro(Trainer_MemberDTO trainer_memberDTO){
+		System.out.println("Trainer_MemberController insertPro()");
+		trainer_memberService.insertTrainer(trainer_memberDTO);
+		return "redirect:null"; // insert 후 갈 주소 입력 !
+		
+	}
+	
+
+}
---
+++ team2_git/src/main/java/com/itwill/domain/Trainer_MemberDTO.java
@@ -0,0 +1,103 @@
+package com.itwill.domain;
+
+public class Trainer_MemberDTO {
+	private String t_id;
+	private String t_pass;
+	private String t_name;
+	private String t_nic;
+	private String t_postcode;
+	private String t_address;
+	private String t_detailAddress;
+	private String t_phone;
+	private String t_email;
+	private int t_cash;
+	private String t_introduce;
+	private String t_exp;
+	private String t_sns;
+	private String t_program;
+	
+	public String getT_id() {
+		return t_id;
+	}
+	public void setT_id(String t_id) {
+		this.t_id = t_id;
+	}
+	public String getT_pass() {
+		return t_pass;
+	}
+	public void setT_pass(String t_pass) {
+		this.t_pass = t_pass;
+	}
+	public String getT_name() {
+		return t_name;
+	}
+	public void setT_name(String t_name) {
+		this.t_name = t_name;
+	}
+	public String getT_nic() {
+		return t_nic;
+	}
+	public void setT_nic(String t_nic) {
+		this.t_nic = t_nic;
+	}
+	public String getT_postcode() {
+		return t_postcode;
+	}
+	public void setT_postcode(String t_postcode) {
+		this.t_postcode = t_postcode;
+	}
+	public String getT_address() {
+		return t_address;
+	}
+	public void setT_address(String t_address) {
+		this.t_address = t_address;
+	}
+	public String getT_detailAddress() {
+		return t_detailAddress;
+	}
+	public void setT_detailAddress(String t_detailAddress) {
+		this.t_detailAddress = t_detailAddress;
+	}
+	public String getT_phone() {
+		return t_phone;
+	}
+	public void setT_phone(String t_phone) {
+		this.t_phone = t_phone;
+	}
+	public String getT_email() {
+		return t_email;
+	}
+	public void setT_email(String t_email) {
+		this.t_email = t_email;
+	}
+	public int getT_cash() {
+		return t_cash;
+	}
+	public void setT_cash(int t_cash) {
+		this.t_cash = t_cash;
+	}
+	public String getT_introduce() {
+		return t_introduce;
+	}
+	public void setT_introduce(String t_introduce) {
+		this.t_introduce = t_introduce;
+	}
+	public String getT_exp() {
+		return t_exp;
+	}
+	public void setT_exp(String t_exp) {
+		this.t_exp = t_exp;
+	}
+	public String getT_sns() {
+		return t_sns;
+	}
+	public void setT_sns(String t_sns) {
+		this.t_sns = t_sns;
+	}
+	public String getT_program() {
+		return t_program;
+	}
+	public void setT_program(String t_program) {
+		this.t_program = t_program;
+	}
+}
